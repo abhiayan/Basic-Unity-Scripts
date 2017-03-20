421f8e24656211455337395e797ae3aa4801e34e
Abhinav Ayan <abhiayan@gmail.com>
2017-03-20 17:23:20 +0530

    Add files via upload

---
+++ BallSpawner.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallSpawner : MonoBehaviour {
+
+	public GameObject ballPrefab;
+
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (ballPrefab);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			GameObject instance = Instantiate (ballPrefab);
+			Rigidbody rb = instance.GetComponent<Rigidbody>();
+			rb.velocity = Vector3.left * 25f ;
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			GameObject instance = Instantiate (ballPrefab);
+			Rigidbody rb = instance.GetComponent<Rigidbody>();
+			rb.velocity = Vector3.right* 25f ;
+		}
+		if (Input.GetButtonDown("Fire1")) {
+			GameObject instance = Instantiate (ballPrefab);
+			instance.transform.position = transform.position; //transform.position will give location of the game object on which this script is applied which in this happens to be the camera.
+			Rigidbody rb = instance.GetComponent<Rigidbody>();
+			rb.velocity = Vector3.forward*25f ;
+		}
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			GameObject instance = Instantiate (ballPrefab);
+			Rigidbody rb = instance.GetComponent<Rigidbody>();
+			rb.velocity = Vector3.back*25f ;
+		}
+	
+	}
+}
---
+++ CameraRotation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float mouseX = Input.GetAxis ("Mouse X");
+		float mouseY = Input.GetAxis ("Mouse Y");
+		transform.localRotation = transform.localRotation * Quaternion.Euler (0, mouseX*10f, 0);
+		Camera camera = GetComponentInChildren<Camera>();
+		camera.transform.localRotation = camera.transform.localRotation * Quaternion.Euler (-mouseY* 10f, 0, 0) ; 
+	
+	}
+}
