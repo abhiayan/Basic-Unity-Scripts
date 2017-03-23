95b7a4880fd8dc6d01bcb862bef9cec46dc5a8c6
Abhinav Ayan <abhiayan@gmail.com>
2017-03-24 00:28:15 +0530

    Add files via upload

---
+++ PrimaryTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrimaryTrigger : MonoBehaviour {
+	
+	void OnTriggerEnter(Collider collider)
+	{
+		print ("abc");
+		SecondaryTrigger trigger = GetComponentInChildren<SecondaryTrigger>();
+		trigger.ExpectCollider(collider);
+	}
+}
+
