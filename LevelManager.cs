8f0fe9b4742e23c224805f42e767d5c36d9460b1
Abhinav Ayan <abhiayan@gmail.com>
2017-03-29 23:04:30 +0530

    Add files via upload

---
+++ LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public float timeTillNextLevel = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeTillNextLevel -= Time.deltaTime;
+
+		if (timeTillNextLevel<0) {
+			LoadNextScene ();
+		}
+
+	}
+
+		public void LoadNextScene()
+		{
+			int currentIndex = SceneManager.GetActiveScene ().buildIndex;
+		if (currentIndex == 2) {
+			SceneManager.LoadScene (currentIndex - 1);
+		}
+		else if (currentIndex != 2) {
+			SceneManager.LoadScene (currentIndex + 1);
+
+		}
+		}
+}
